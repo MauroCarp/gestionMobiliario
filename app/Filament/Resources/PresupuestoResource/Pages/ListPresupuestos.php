e7b0a13ac5ffcba0a5e6ab23b7634c9021c33bfe
Mauro <maurogt.dd@gmail.com>
2026-06-17 13:10:15 -0300

    Agrego contador a tabs de filtros

--- app/Filament/Resources/PresupuestoResource/Pages/ListPresupuestos.php
+++ app/Filament/Resources/PresupuestoResource/Pages/ListPresupuestos.php
@@ -21,12 +21,23 @@ class ListPresupuestos extends ListRecords
 
     public function getTabs(): array
     {
+        $baseQuery = static::getResource()::getEloquentQuery();
+
+        $counts = (clone $baseQuery)
+            ->selectRaw('estado, count(*) as aggregate')
+            ->groupBy('estado')
+            ->pluck('aggregate', 'estado');
+            
+        $total = (clone $baseQuery)->count();
+        
         return [
-            'todos'       => Tab::make('Todos'),
-            'borrador'    => Tab::make('Borrador')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'borrador')),
-            'en_revision' => Tab::make('En Revisión')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'en_revision')),
-            'aprobado'    => Tab::make('Aprobados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'aprobado')),
-            'rechazado'   => Tab::make('Rechazados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'rechazado')),
+            'todos'       => Tab::make('Todos')->badge($total),
+            'borrador'    => Tab::make('Borrador')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'borrador'))->badge(($counts['borrador']) ? $counts['borrador'] : 0),
+            'en_revision' => Tab::make('En Revisión')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'en_revision'))->badge(isset($counts['en_revision']) ? $counts['en_revision'] : 0),
+            'aprobado'    => Tab::make('Aprobados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'aprobado'))->badge(isset($counts['aprobado']) ? $counts['aprobado'] : 0),
+            'confirmado'    => Tab::make('Confirmados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'confirmado'))->badge(isset($counts['confirmado']) ? $counts['confirmado'] : 0),
+            'pagado'    => Tab::make('Pagados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'pagado'))->badge(isset($counts['pagado']) ? $counts['pagado'] : 0),
+            'rechazado'   => Tab::make('Rechazados')->modifyQueryUsing(fn (Builder $query) => $query->where('estado', 'rechazado'))->badge(isset($counts['rechazado']) ? $counts['rechazado'] : 0),
         ];
     }
 }
